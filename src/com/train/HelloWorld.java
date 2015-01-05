975c272a2b4f67a2960b90a86e0bd9a7b5b9a0ec
lw梁卫 <weiliang@Ctrip.com>
2015-01-05 15:43:52 +0800

    add

---
+++ src/com/train/HelloWorld.java
@@ -0,0 +1,17 @@
+package com.train;
+
+/**
+ * @author weiliang
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
